a92c697d44d3532633b07e3ee00f449f1f82cb16
Neeraj Gangwar <y.neeraj2008@gmail.com>
2016-08-18 16:21:19 +0530

    remove: unnecessary sbt plugins and resolvers

--- project/CassieBuild.scala
+++ project/CassieBuild.scala
@@ -2,12 +2,7 @@ import sbt._
 import sbt.Classpaths.publishTask
 import Keys._
 
-import com.typesafe.sbt.SbtMultiJvm
-import com.typesafe.sbt.SbtMultiJvm.MultiJvmKeys.{ MultiJvm, extraOptions, jvmOptions, scalatestOptions, multiNodeExecuteTests, multiNodeJavaName, multiNodeHostsFileName, multiNodeTargetDirName, multiTestOptions }
 import com.typesafe.sbt.packager.archetypes.JavaAppPackaging
-
-import com.typesafe.sbt.SbtScalariform
-import com.typesafe.sbt.SbtScalariform.ScalariformKeys
 
 import sbtassembly.AssemblyPlugin.autoImport._
 
@@ -21,7 +16,7 @@ object CassieBuild extends Build with StandardLibraries {
 
   def sharedSettings = Seq(
     organization := "com.aianonymous",
-    version := "0.2.0",
+    version := "0.1.0",
     scalaVersion := Version.scala,
     crossScalaVersions := Seq(Version.scala, "2.10.6"),
     scalacOptions := Seq("-unchecked", "-optimize", "-deprecation", "-feature", "-language:higherKinds", "-language:implicitConversions", "-language:postfixOps", "-language:reflectiveCalls", "-Yinline-warnings", "-encoding", "utf8"),
