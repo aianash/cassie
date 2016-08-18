5b8d4a959121973bc380a264a2fb81dfee608ba8
Neeraj Gangwar <y.neeraj2008@gmail.com>
2016-08-18 16:01:06 +0530

    add: setup build for cassie

---
+++ project/CassieBuild.scala
@@ -0,0 +1,44 @@
+import sbt._
+import sbt.Classpaths.publishTask
+import Keys._
+
+import com.typesafe.sbt.SbtMultiJvm
+import com.typesafe.sbt.SbtMultiJvm.MultiJvmKeys.{ MultiJvm, extraOptions, jvmOptions, scalatestOptions, multiNodeExecuteTests, multiNodeJavaName, multiNodeHostsFileName, multiNodeTargetDirName, multiTestOptions }
+import com.typesafe.sbt.packager.archetypes.JavaAppPackaging
+
+import com.typesafe.sbt.SbtScalariform
+import com.typesafe.sbt.SbtScalariform.ScalariformKeys
+
+import sbtassembly.AssemblyPlugin.autoImport._
+
+import com.typesafe.sbt.SbtNativePackager._, autoImport._
+import com.typesafe.sbt.packager.Keys._
+import com.typesafe.sbt.packager.docker.{Cmd, ExecCmd, CmdLike}
+
+import com.aianonymous.sbt.standard.libraries.StandardLibraries
+
+object CassieBuild extends Build with StandardLibraries {
+
+  def sharedSettings = Seq(
+    organization := "com.aianonymous",
+    version := "0.2.0",
+    scalaVersion := Version.scala,
+    crossScalaVersions := Seq(Version.scala, "2.10.6"),
+    scalacOptions := Seq("-unchecked", "-optimize", "-deprecation", "-feature", "-language:higherKinds", "-language:implicitConversions", "-language:postfixOps", "-language:reflectiveCalls", "-Yinline-warnings", "-encoding", "utf8"),
+    retrieveManaged := true,
+
+    fork := true,
+    javaOptions += "-Xmx2500M",
+
+    resolvers ++= StandardResolvers,
+
+    publishMavenStyle := true
+  ) ++ net.virtualvoid.sbt.graph.Plugin.graphSettings
+
+  lazy val cassie = Project(
+    id = "cassie",
+    base = file("."),
+    settings = Project.defaultSettings
+  ).aggregate()
+
+}
